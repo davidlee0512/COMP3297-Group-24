fa1e3faec40b667ed16ea023904bc192bf06c0a2
davidlee0512 <44993546+davidlee0512@users.noreply.github.com>
2018-11-17 21:57:30 +0800

    minor change and add comment

--- project/login/views.py
+++ project/login/views.py
@@ -5,12 +5,16 @@ from django.views.generic.list import ListView
 from login.models import *
 
 # Create your views here.
+
+#login page
 def mainpage(request):
     return render(request, 'login.html', {})
 
+#forget password page
 def forgetPassword(request):
     return render(request, 'forgotPassword.html', {})
 
+#registration page
 class registration(ListView):
     model = Location
     template_name = "registration.html"
@@ -18,19 +22,28 @@ class registration(ListView):
     def get_queryset(self):
         return super().get_queryset().exclude(id = 1)
 
+#profile page
 class Profile(ListView):
     model = User
     template_name = "profile.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        username = self.kwargs['username']
+
+        #get the data form urls(done in urls.py)
+        username = self.kwargs['username'] 
         password = self.kwargs['password']
+
+        #get the user data from database base of username
         user = User.objects.get(userID = username)
+
+        #check the page if the username and password is correct
         if (user.password == password):
             context['correct'] = True
         else:
             context['correct'] = False
+
+        #put the data from the database to the page for render the page
         context["user"] = user
         return context
 
@@ -42,7 +55,11 @@ class clinicManagerItem(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+
+        #get the data from url(done in urls.py)
         id_ = self.kwargs['userid']
+
+        #get the object for render
         context["user"] = User.objects.get(id = id_)
         return context
 
@@ -52,8 +69,12 @@ class clinicManagerDescription(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+
+        #get the data from urls(done in urls.py)
         itemid = self.kwargs['itemid']
         userid = self.kwargs['userid']
+
+        #get the object for render
         context["item"] = Item.objects.get(id = itemid)
         context["user"] = User.objects.get(id = userid)
         return context
@@ -66,7 +87,11 @@ class clinicManagerOrder(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+
+        #get the data from urls(done in urls.py)
         id_ = self.kwargs['userid']
+
+        #get the object for render
         context["user"] = User.objects.get(id = id_)
         context["orders"] = Order.objects.filter(location = context["user"].clinic)
         return context
@@ -84,7 +109,11 @@ class warehousePersonalChecklist(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+
+        #get the data from urls(done in urls.py)
         orderid = self.kwargs['orderid']
+
+        #get the object for render
         context["order"] = Order.objects.get(id = orderid)
         return context
 
@@ -96,44 +125,74 @@ class dispatcherOrder(ListView):
         return super().get_queryset().filter(status = 'Queued for Dispatch')
 
 def changeProfile(request, ID, password, email, firstname, lastname):
+    #get the user object that need to change profile
     user = User.objects.get(id = ID)
+
+    #update the data base on the input
     user.password = password
     user.email = email
     user.firstname = firstname
     user.lastname = lastname
+
+    #update the database
     user.save()
+
     return HttpResponse('Change Applied')
 
 def makeOrder(request, userid, itemid, quantity):
+    #get the object that for make a new order
     item = Item.objects.get(id = itemid)
     user = User.objects.get(id = userid)
+
+    #create a new order object
     order = Order()
     order.status = "Queued for Processing"
     order.location = user.clinic
     order.combined_weights = item.shipping_weight * quantity
     order.priority = 10
+
+    #save the order to database
     order.save()
+
+    #create a Order_Item object (intermediate object)
     set_ = Order_Item()
     set_.order_id = order.id
     set_.item_id = item.id
     set_.quantities = quantity
+
+    #save the Order_Item object
     set_.save()
+
     return HttpResponse('Order successful')
 
 def deleteOrder(request, userid, orderid):
+
+    #delete the order
     order = Order.objects.get(id = orderid)
     order.delete()
+
     return HttpResponse('Order deleted')
 
 def pack(request, orderid):
+    #get the order object
     order = Order.objects.get(id = orderid)
+
+    #update the status
     order.status = 'Queued for Dispatch'
+
+    #save the object
     order.save()
+
     return HttpResponse('Packed')
 
 def dispatch(request, orderid):
+    #get the order object
     order = Order.objects.get(id = orderid)
+
+    #update the status
     order.status = 'Dispatched'
+
+    #save the object
     order.save()
     return HttpResponse('Dispatched')
 
