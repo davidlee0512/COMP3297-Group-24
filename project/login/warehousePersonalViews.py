381a1a42fb9092bc6982fe1be20bb69ecc6b5d6c
Felixxdd <u3537471@connect.hku.hk>
2018-12-01 05:31:16 +0800

    Update warehousePersonalViews.py

--- project/login/warehousePersonalViews.py
+++ project/login/warehousePersonalViews.py
@@ -36,13 +36,15 @@ class warehousePersonalChecklist(ListView):
         context = super().get_context_data(**kwargs)
 
         #get the data from urls(done in urls.py)
-        orderid = self.request.POST["orderId"]
+         orderid = self.kwargs['orderid']
 
         #get the object for render
         context["order"] = Order.objects.get(id = orderid)
         return context
     def post(self, request, *args, **kwargs):
-        return self.as_view(request)
+        orderid = request.POST["orderId"]
+        context["order"] = Order.objects.get(id = orderid)
+        return context
 
 def processOrder(request):
     #get the order object
